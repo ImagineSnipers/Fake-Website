d4293f4ab6bd2dca8e7e01b0289f1cb03a8fc4fe
PlugWorld <parkersthinking@gmail.com>
2022-05-13 14:24:04 -0600

    aaaa

--- index.php
+++ index.php
@@ -19,6 +19,26 @@
 		<link href="styles/headers.css" rel="stylesheet">
 		<link href="styles/heroes.css" rel="stylesheet">
 		<link href="styles/features.css" rel="stylesheet">
+		
+		<script>
+			alert("HAAA");
+		
+			<?php
+				include "database/connection.php";
+				$con = openCon();
+			?>
+
+			var joke_questions = "<?php 
+				$sql = "SELECT JokeQuestion FROM JokeSuggestions;";
+				$query = mysqli_query($con, $sql);
+				$value_array = array();
+				//while($row = mysql_fetch_array($query)){
+				//	$value_array[] = $row['JokeQuestion'];
+				//}
+				//$collection = implode(", ", $value_array);
+				//echo $collection;
+			?>");
+		</script>
 	</head>
 	<body>
 		<div class="container">
@@ -41,18 +61,15 @@
 			<img class="d-block mx-auto mb-4" src="favicons/safari-pinned-tab.svg" alt="" width="72" height="57">
 			<h1 class="display-5 fw-bold">Dad Jokes?</h1>
 			<div class="col-lg-6 mx-auto">
-				<p class="lead mb-4">Dad jokes are here to make the world a better place.</p>
-				<p class="lead mb-4">One dad joke at a time.</p>
+				<div class="pt-3 pb-4">
+					<p id="joke-question" class="lead h3">I broke my arm in two places.</p>
+					<p id="joke-answer" class="lead h2"><strong>My doctor told me to stop going to those places.</strong></p>
+				</div>
 				<div class="d-grid gap-2 d-sm-flex justify-content-sm-center pb-3">
 					<a href="pages/joke_form.html" class="btn btn-primary btn-lg px-4 gap-3" role="button">Submit Joke</a>
 					<a href="pages/contact.html" class="btn btn-outline-secondary btn-lg px-4" role="button">Contact</a>
 					<!--<button type="button" class="btn btn-primary btn-lg px-4 gap-3">Learn More</button>
 					<button type="button" class="btn btn-outline-secondary btn-lg px-4">Contact</button>-->
-				</div>
-				<hr>
-				<div class="pt-3">
-					<p id="joke-question" class="lead h3">I broke my arm in two places.</p>
-					<p id="joke-answer" class="lead h2"><strong>My doctor told me to stop going to those places.</strong></p>
 				</div>
 			</div>
 		</div>
